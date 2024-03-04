9820a8524574044a3542e21fd6b4a2e2874d1c11
cristhian <cristhian.espinosa@dowjones.com>
2024-03-04 17:22:19 -0500

    modify desing google login

--- composeApp/src/commonMain/kotlin/screens/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/LoginScreen.kt
@@ -1,12 +1,14 @@
-package screens
-
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -14,17 +16,20 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
+import androidx.compose.ui.unit.sp
 import login.SignInViewModel
 import navigation.ScreenLoginComponent
 import navigation.ScreenLoginEvent
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.koinInject
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
-fun LoginScreen(component: ScreenLoginComponent,  signInViewModel: SignInViewModel = koinInject<SignInViewModel>()
-) {
-    val text by component.text.subscribeAsState()
+fun LoginScreen(component: ScreenLoginComponent, signInViewModel: SignInViewModel = koinInject()) {
     val signInState by signInViewModel.state.collectAsState()
 
     LaunchedEffect(signInState) {
@@ -34,22 +39,34 @@ fun LoginScreen(component: ScreenLoginComponent,  signInViewModel: SignInViewMod
         }
     }
 
-    Column(
-        Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
+    Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
+        Column(
+            Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter =  painterResource("google.png"),
+                contentDescription = "Google logo"
+            )
 
-        Text("Login")
-        OutlinedTextField(
-            value = text,
-            onValueChange = { component.onEvent(ScreenLoginEvent.UpdateText(it)) },
-            modifier = Modifier.fillMaxWidth().padding(16.dp)
-        )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = {
-            component.onEvent(ScreenLoginEvent.ClickButtonLogin)
-        }) {
-            Text("Login")
+            Button(
+                onClick = { component.onEvent(ScreenLoginEvent.ClickButtonLogin) },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF4285F4)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp)
+                    .height(50.dp)
+            ) {
+                Text(
+                    "Login with Google",
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
         }
     }
 }
